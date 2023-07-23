3b015e43b52b28c679ce6da8c91d36bb34d7a023
Lu Ji <lu.ji1@me.com>
2023-07-22 21:02:33 -0400

    fix link with # hashtags

--- theme/patterns/skills.php
+++ theme/patterns/skills.php
@@ -8,11 +8,11 @@
 
 <div class="is-layout-constrained">
 	<div class="skills is-layout-flex">
-		<a class="skill" href="/projects#personal-work" style="background-color: #BD303C;">
+		<a class="skill" href="/projects/#personal-work" style="background-color: #BD303C;">
 			<div class="icon" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/assets/images/skills/my-avatar.png)' ?>;"></div>
 			<div class="title">Software Engineer</div>
 		</a>
-		<a class="skill" href="/projects#design" style="background-color: #F35426;">
+		<a class="skill" href="/projects/#design" style="background-color: #F35426;">
 			<div class="icon" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/assets/images/skills/designer.png)' ?>;border-radius:0;background-color:transparent;"></div>
 			<div class="title">Designer</div>
 		</a>
@@ -24,11 +24,11 @@
 			<div class="icon" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/assets/images/skills/teacher.png)' ?>;"></div>
 			<div class="title">Educator</div>
 		</a>
-		<a class="skill" href="/projects#DIY" style="background-color: #E8A342;">
+		<a class="skill" href="/projects/#DIY" style="background-color: #E8A342;">
 			<div class="icon" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/assets/images/skills/DIYer.png)' ?>;"></div>
 			<div class="title">DIY Builder</div>
 		</a>
-		<a class="skill" href="/projects#oss" style="background-color: #643793;">
+		<a class="skill" href="/projects/#oss" style="background-color: #643793;">
 			<div class="icon" style="background-image: url(<?php echo get_stylesheet_directory_uri() . '/assets/images/skills/github.webp)' ?>;"></div>
 			<div class="title">OSS Contributer</div>
 		</a>
