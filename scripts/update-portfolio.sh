8d81db785514ab8c053ce93b6c412d5b93e5bcfc
Lu Ji <lu.ji1@me.com>
2023-07-23 01:09:57 -0400

    * chown after copying theme dir

--- scripts/update-portfolio.sh
+++ scripts/update-portfolio.sh
@@ -2,4 +2,6 @@
 
 rm -rf /var/www/html/wp-content/themes/jjpro-portfolio
 cp -Rf /app /var/www/html/wp-content/themes/jjpro-portfolio
+chown -R 33:33 /var/www/html/wp-content/themes/jjpro-portfolio
+
 exec $@
